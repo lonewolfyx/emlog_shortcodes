6345e33150e155b0cf000ad5fe4ecc62c9dc4da0
OldDriver <olddrivero.king@qq.com>
2021-01-30 22:28:34 +0800

    Create shortcodes.php

---
+++ shortcodes/shortcodes.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Emlog 短代码
+Version: 1.0
+Plugin URL:https://github.com/lonewolfyx/emlog_shortcodes
+Description: 使用过 WordPress 都知道，强大的短代码为用户提供了强大的功能，那么它来了。
+Author: MrTang
+Author URL: https://wtool.com.cn/
+*/
+
+!defined('EMLOG_ROOT') && exit('access deined!');

5f378c1195ed106ebf6461180c68a7986b175d09
OldDriver <olddrivero.king@qq.com>
2021-04-23 15:05:47 +0800

    Update shortcodes.php

--- shortcodes/shortcodes.php
+++ shortcodes/shortcodes.php
@@ -9,3 +9,429 @@ Author URL: https://wtool.com.cn/
 */
 
 !defined('EMLOG_ROOT') && exit('access deined!');
+
+/**
+ * Emlog 短代码
+ *
+ * @date    2021-01-07 21:22:35
+ * @author  MrTang
+ * @mail    olddrivero.king@qq.com
+ *
+ * A few examples are below:
+ *
+ * [shortcode /]
+ * [shortcode foo="bar" baz="bing" /]
+ * [shortcode foo="bar"]content[/shortcode]
+ *
+ * $out = do_shortcode( $content );
+ *
+ */
+ 
+/**
+ * 全局短代码标签
+ */
+$shortcode_tags = array();
+
+
+/**
+ * 增加一个新的短代码
+ */
+function add_shortcode( $tag, $callback ){
+	global $shortcode_tags;
+
+	if ( '' === trim( $tag ) ) {
+		return;
+	}
+
+	if ( 0 !== preg_match( '@[<>&/\[\]\x00-\x20=]@', $tag ) ) {
+		return;
+	}
+	
+	$shortcode_tags[ $tag ] = $callback;
+	
+}
+
+/**
+ * 移除短代码
+ */
+function remove_shortcode( $tag ) {
+	global $shortcode_tags;
+
+	unset( $shortcode_tags[ $tag ] );
+}
+
+/**
+ * 移除所有短代码
+ */
+function remove_all_shortcodes() {
+	global $shortcode_tags;
+
+	$shortcode_tags = array();
+}
+
+/**
+ * 检测是否存在某短代码
+ */
+function shortcode_exists( $tag ) {
+	global $shortcode_tags;
+	return array_key_exists( $tag, $shortcode_tags );
+}
+
+
+/**
+ * 判断传递的内容中是否包含某指定短代码
+ */
+function has_shortcode( $content, $tag ) {
+	if ( false === strpos( $content, '[' ) ) {
+		return false;
+	}
+
+	if ( shortcode_exists( $tag ) ) {
+		preg_match_all( '/' . get_shortcode_regex() . '/', $content, $matches, PREG_SET_ORDER );
+		if ( empty( $matches ) ) {
+			return false;
+		}
+
+		foreach ( $matches as $shortcode ) {
+			if ( $tag === $shortcode[2] ) {
+				return true;
+			} elseif ( ! empty( $shortcode[5] ) && has_shortcode( $shortcode[5], $tag ) ) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+/**
+ * 执行短代码，并进行过滤
+ */
+function do_shortcode($content, $ignore_html = false){
+	global $shortcode_tags;
+	
+	if ( false === strpos( $content, '[' ) ) {
+		return $content;
+	}
+
+	if ( empty( $shortcode_tags ) || ! is_array( $shortcode_tags ) ) {
+		return $content;
+	}
+	
+	preg_match_all( '@\[([^<>&/\[\]\x00-\x20=]++)@', $content, $matches );
+	$tagnames = array_intersect( array_keys( $shortcode_tags ), $matches[1] );
+
+	if ( empty( $tagnames ) ) {
+		return $content;
+	}
+	
+	/*
+	$_loc1_ = array(
+		'&#91;' => '&#091;',
+		'&#93;' => '&#093;',
+	);
+	
+	$_loc2_ = strtr( $content, $_loc1_ );
+	
+	$_loc3_   = array(
+		'[' => '&#91;',
+		']' => '&#93;',
+	);
+	
+	$_loc4_ = array(
+		'&#91;' => '[',
+		'&#93;' => ']',
+	);
+	
+	$_loc5_ = preg_split( "/(<(?(?=!--|!\[CDATA\[)(?(?=!-)!(?:-(?!->)[^\-]*+)*+(?:-->)?|!\[CDATA\[[^\]]*+(?:](?!]>)[^\]]*+)*+(?:]]>)?)|[^>]*>?))/", $content, -1, PREG_SPLIT_DELIM_CAPTURE );
+	$_loc6_ = implode( '', $_loc5_ );
+	
+	
+	$_loc7_ = get_shortcode_regex( $tagnames );
+	$_loc8_ = preg_replace_callback( "/$_loc7_/", 'do_shortcode_tag', $_loc6_ );
+	
+	$_loc9_ = strtr( $_loc8_, $_loc4_ );
+	
+	return $_loc9_;
+	*/
+	$content = do_shortcodes_in_html_tags( $content, $ignore_html, $tagnames );
+
+	$pattern = get_shortcode_regex( $tagnames );
+	$content = preg_replace_callback( "/$pattern/", 'do_shortcode_tag', $content );
+
+	$content = unescape_invalid_shortcodes( $content );
+
+	return $content;
+}
+
+
+/**
+ * 获取所有短代码注册正则
+ */
+function get_shortcode_regex( $tagnames = null ) {
+	global $shortcode_tags;
+
+	if ( empty( $tagnames ) ) {
+		$tagnames = array_keys( $shortcode_tags );
+	}
+	$tagregexp = implode( '|', array_map( 'preg_quote', $tagnames ) );
+
+	return '\\['                             // Opening bracket.
+		. '(\\[?)'                           // 1: Optional second opening bracket for escaping shortcodes: [[tag]].
+		. "($tagregexp)"                     // 2: Shortcode name.
+		. '(?![\\w-])'                       // Not followed by word character or hyphen.
+		. '('                                // 3: Unroll the loop: Inside the opening shortcode tag.
+		.     '[^\\]\\/]*'                   // Not a closing bracket or forward slash.
+		.     '(?:'
+		.         '\\/(?!\\])'               // A forward slash not followed by a closing bracket.
+		.         '[^\\]\\/]*'               // Not a closing bracket or forward slash.
+		.     ')*?'
+		. ')'
+		. '(?:'
+		.     '(\\/)'                        // 4: Self closing tag...
+		.     '\\]'                          // ...and closing bracket.
+		. '|'
+		.     '\\]'                          // Closing bracket.
+		.     '(?:'
+		.         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags.
+		.             '[^\\[]*+'             // Not an opening bracket.
+		.             '(?:'
+		.                 '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag.
+		.                 '[^\\[]*+'         // Not an opening bracket.
+		.             ')*+'
+		.         ')'
+		.         '\\[\\/\\2\\]'             // Closing shortcode tag.
+		.     ')?'
+		. ')'
+		. '(\\]?)';                          // 6: Optional second closing brocket for escaping shortcodes: [[tag]].
+}
+
+
+/**
+ * 执行注册短代码函数驱动
+ */
+function do_shortcode_tag( $m ) {
+	global $shortcode_tags;
+
+
+	if ( '[' === $m[1] && ']' === $m[6] ) {
+		return substr( $m[0], 1, -1 );
+	}
+
+	$tag  = $m[2];
+	$attr = shortcode_parse_atts( $m[3] );
+
+	if ( ! is_callable( $shortcode_tags[ $tag ] ) ) {
+		
+		return $m[0];
+	}
+	
+	$content = isset( $m[5] ) ? $m[5] : null;
+
+	$output = $m[1] . call_user_func( $shortcode_tags[ $tag ], $attr, $content, $tag ) . $m[6];
+
+	return $output;
+}
+
+/**
+ * 搜索在 HTML 产生的短代码
+ */
+function do_shortcodes_in_html_tags( $content, $ignore_html, $tagnames ) {
+	$trans   = array(
+		'&#91;' => '&#091;',
+		'&#93;' => '&#093;',
+	);
+	$content = strtr( $content, $trans );
+	$trans   = array(
+		'[' => '&#91;',
+		']' => '&#93;',
+	);
+	
+	$pattern = get_shortcode_regex( $tagnames );
+	$textarr = preg_split( get_html_split_regex(), $content, -1, PREG_SPLIT_DELIM_CAPTURE );
+	
+	$content = implode( '', $textarr );
+
+	return $content;
+}
+
+/**
+ * 检索HTML元素的正则表达式。
+ */
+function get_html_split_regex() {
+    static $regex;
+ 
+    if ( ! isset( $regex ) ) {
+        // phpcs:disable Squiz.Strings.ConcatenationSpacing.PaddingFound -- don't remove regex indentation
+        $comments =
+            '!'             // Start of comment, after the <.
+            . '(?:'         // Unroll the loop: Consume everything until --> is found.
+            .     '-(?!->)' // Dash not followed by end of comment.
+            .     '[^\-]*+' // Consume non-dashes.
+            . ')*+'         // Loop possessively.
+            . '(?:-->)?';   // End of comment. If not found, match all input.
+ 
+        $cdata =
+            '!\[CDATA\['    // Start of comment, after the <.
+            . '[^\]]*+'     // Consume non-].
+            . '(?:'         // Unroll the loop: Consume everything until ]]> is found.
+            .     '](?!]>)' // One ] not followed by end of comment.
+            .     '[^\]]*+' // Consume non-].
+            . ')*+'         // Loop possessively.
+            . '(?:]]>)?';   // End of comment. If not found, match all input.
+ 
+        $escaped =
+            '(?='             // Is the element escaped?
+            .    '!--'
+            . '|'
+            .    '!\[CDATA\['
+            . ')'
+            . '(?(?=!-)'      // If yes, which type?
+            .     $comments
+            . '|'
+            .     $cdata
+            . ')';
+ 
+        $regex =
+            '/('                // Capture the entire match.
+            .     '<'           // Find start of element.
+            .     '(?'          // Conditional expression follows.
+            .         $escaped  // Find end of escaped element.
+            .     '|'           // ...else...
+            .         '[^>]*>?' // Find end of normal element.
+            .     ')'
+            . ')/';
+        // phpcs:enable
+    }
+ 
+    return $regex;
+}
+
+/**
+ * 删除短代码中存在的占位符
+ */
+function unescape_invalid_shortcodes( $content ) {
+	// Clean up entire string, avoids re-parsing HTML.
+	$trans = array(
+		'&#91;' => '[',
+		'&#93;' => ']',
+	);
+
+	$content = strtr( $content, $trans );
+
+	return $content;
+}
+
+/**
+ * 短代码正则分析表
+ */
+function get_shortcode_atts_regex() {
+	return '/([\w-]+)\s*=\s*"([^"]*)"(?:\s|$)|([\w-]+)\s*=\s*\'([^\']*)\'(?:\s|$)|([\w-]+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|\'([^\']*)\'(?:\s|$)|(\S+)(?:\s|$)/';
+}
+
+/**
+ * 从shortcodes标记检索所有属性。。
+ */
+function shortcode_parse_atts( $text ) {
+	$atts    = array();
+	$text    = preg_replace( "/[\x{00a0}\x{200b}]+/u", ' ', $text );
+	if ( preg_match_all( '/([\w-]+)\s*=\s*"([^"]*)"(?:\s|$)|([\w-]+)\s*=\s*\'([^\']*)\'(?:\s|$)|([\w-]+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|\'([^\']*)\'(?:\s|$)|(\S+)(?:\s|$)/', $text, $match, PREG_SET_ORDER ) ) {
+		foreach ( $match as $m ) {
+			if ( ! empty( $m[1] ) ) {
+				$atts[ strtolower( $m[1] ) ] = stripcslashes( $m[2] );
+			} elseif ( ! empty( $m[3] ) ) {
+				$atts[ strtolower( $m[3] ) ] = stripcslashes( $m[4] );
+			} elseif ( ! empty( $m[5] ) ) {
+				$atts[ strtolower( $m[5] ) ] = stripcslashes( $m[6] );
+			} elseif ( isset( $m[7] ) && strlen( $m[7] ) ) {
+				$atts[] = stripcslashes( $m[7] );
+			} elseif ( isset( $m[8] ) && strlen( $m[8] ) ) {
+				$atts[] = stripcslashes( $m[8] );
+			} elseif ( isset( $m[9] ) ) {
+				$atts[] = stripcslashes( $m[9] );
+			}
+		}
+
+		foreach ( $atts as &$value ) {
+			if ( false !== strpos( $value, '<' ) ) {
+				if ( 1 !== preg_match( '/^[^<]*+(?:<[^>]*+>[^<]*+)*+$/', $value ) ) {
+					$value = '';
+				}
+			}
+		}
+	} else {
+		$atts = ltrim( $text );
+	}
+
+	return $atts;
+}
+
+/**
+ * 短代码默认值匹配
+ */
+function shortcode_atts( $pairs, $atts ) {
+	$atts = (array) $atts;
+	$out  = array();
+	foreach ( $pairs as $name => $default ) {
+		if ( array_key_exists( $name, $atts ) ) {
+			$out[ $name ] = $atts[ $name ];
+		} else {
+			$out[ $name ] = $default;
+		}
+	}
+	return $out;
+}
+
+/**
+ * 删除内容中注册的短代码
+ */
+function hide_shortcode( $content ){
+	return strip_shortcodes( $content );
+}
+
+/**
+ * 移除所有在内容中存在的短代码
+ */
+function strip_shortcodes( $content ) {
+	global $shortcode_tags;
+
+	if ( false === strpos( $content, '[' ) ) {
+		return $content;
+	}
+
+	if ( empty( $shortcode_tags ) || ! is_array( $shortcode_tags ) ) {
+		return $content;
+	}
+
+	// 寻找所有在内容中出现的短代码
+	preg_match_all( '@\[([^<>&/\[\]\x00-\x20=]++)@', $content, $matches );
+
+	$tags_to_remove = array_keys( $shortcode_tags );
+
+	$tagnames = array_intersect( $tags_to_remove, $matches[1] );
+
+	if ( empty( $tagnames ) ) {
+		return $content;
+	}
+
+	$content = do_shortcodes_in_html_tags( $content, true, $tagnames );
+
+	$pattern = get_shortcode_regex( $tagnames );
+	$content = preg_replace_callback( "/$pattern/", 'strip_shortcode_tag', $content );
+
+	$content = unescape_invalid_shortcodes( $content );
+
+	return $content;
+}
+
+/**
+ * 根据正则剥离一个短代码
+ */
+function strip_shortcode_tag( $m ) {
+	// Allow [[foo]] syntax for escaping a tag.
+	if ( '[' === $m[1] && ']' === $m[6] ) {
+		return substr( $m[0], 1, -1 );
+	}
+
+	return $m[1] . $m[6];
+}
